7fa1023345e39ab7ce847208c704bceacfbc7849
wardenlym <wardenlym@gmail.com>
2021-03-23 13:30:29 +0800

    fix

---
+++ modules-load.d.sh
@@ -0,0 +1,9 @@
+       /etc/modules-load.d/bridge.conf:
+
+           br_netfilter
+
+       /etc/sysctl.d/bridge.conf:
+
+           net.bridge.bridge-nf-call-ip6tables = 0
+           net.bridge.bridge-nf-call-iptables = 0
+           net.bridge.bridge-nf-call-arptables = 0
