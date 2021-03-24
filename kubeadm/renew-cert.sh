5c13d7c6c862f87835d438c2f70442bd0b4900aa
wardenlym <wardenlym@gmail.com>
2021-03-25 02:28:50 +0800

    fix

--- kubeadm/renew-cert.sh
+++ kubeadm/renew-cert.sh
@@ -3,6 +3,7 @@ kubeadm alpha certs check-expiration
 kubeadm alpha certs renew all
 
 # 每次只能renew 1年
+# https://mritd.com/2020/01/21/how-to-extend-the-validity-of-your-kubeadm-certificate/
 总结一下，调整 kubeadm 证书期限有两种方案；第一种直接修改源码，耗时耗力还得会 go，最后还要跑跨平台编译(很耗时)；第二种在启动集群时调整 kube-controller-manager 组件的 --experimental-cluster-signing-duration 参数，集群创建好后手动 renew 一下并批准相关 csr。
 
 controllerManager:
