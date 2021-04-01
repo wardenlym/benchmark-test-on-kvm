1766fa37d6d6ec6ec874db7fc936bf8a9e8134c8
wardenlym <wardenlym@gmail.com>
2021-04-01 18:01:40 +0800

    fix

--- pv-pvc/nfs.sh
+++ pv-pvc/nfs.sh
@@ -7,6 +7,28 @@ yum -y install nfs-utils rpcbind
 cat /etc/exports
 /data/nfs    172.29.50.0/24(rw,sync,no_root_squash)
 
+```
+#：允许ip地址范围在192.168.0.*的计算机以读写的权限来访问/home/work 目录。  
+/home/work 192.168.0.*（rw,sync,root_squash）  
+/home  192.168.1.105 (rw,sync)  
+/public  * (rw,sync)  
+
+配置文件每行分为两段：第一段为共享的目录，使用绝对路径，第二段为客户端地址及权限。  
+地址可以使用完整IP或网段，例如10.0.0.8或10.0.0.0/24，10.0.0.0/255.255.255.0当然也可以地址可以使用主机名，DNS解析的和本地/etc/hosts解析的都行，支持通配符，例如：*.chengyongxu.com  
+  
+权限有：  
+rw：read-write，可读写；    注意，仅仅这里设置成读写客户端还是不能正常写入，还要正确地设置共享目录的权限，参考问题7  
+ro：read-only，只读；  
+sync：文件同时写入硬盘和内存；  
+async：文件暂存于内存，而不是直接写入内存；  
+no_root_squash：NFS客户端连接服务端时如果使用的是root的话，那么对服务端分享的目录来说，也拥有root权限。显然开启这项是不安全的。  
+root_squash：NFS客户端连接服务端时如果使用的是root的话，那么对服务端分享的目录来说，拥有匿名用户权限，通常他将使用nobody或nfsnobody身份；  
+all_squash：不论NFS客户端连接服务端时使用什么用户，对服务端分享的目录来说都是拥有匿名用户权限；  
+anonuid：匿名用户的UID值，通常是nobody或nfsnobody，可以在此处自行设定；  
+anongid：匿名用户的GID值。  
+```
+
+
 # 启动服务
 
 systemctl start rpcbind.service
@@ -149,3 +171,8 @@ spec:
           claimName: pvc-28
 EOF
 
+
+helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
+$ helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
+    --set nfs.server=172.29.50.28 \
+    --set nfs.path=/data/nfs
